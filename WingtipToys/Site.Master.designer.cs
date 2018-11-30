03a4f38cb3d9f3b63ee00da424257457ab5e1477
Kevin Stone <a22612999@yahoo.com.tw>
2018-11-30 14:49:51 +0800

    Membership and Administration

--- WingtipToys/Site.Master.designer.cs
+++ WingtipToys/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace WingtipToys {
     public partial class SiteMaster {
         
         /// <summary>
+        /// adminLink 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor adminLink;
+        
+        /// <summary>
         /// cartCount 控制項。
         /// </summary>
         /// <remarks>
